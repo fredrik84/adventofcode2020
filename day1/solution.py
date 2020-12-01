921d65d5ced48a19015d3ea08a296c828cdb95cc
Fredrik Eriksson <fredrik.eriksson@avanza.se>
2020-12-01 23:10:48 +0100

    Day 1, here we go

---
+++ day1/solution.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+print("part 1:", list(map(lambda f: [x*y for x in f for y in f if x+y==2020].pop(), [list(map(int, open('input.txt').read().split()))])).pop())
+
+print("part 2:", list(map(lambda f: [x*y*z for x in f for y in f for z in f if x+y+z==2020].pop(), [list(map(int, open('input.txt').read().split()))])).pop())
+
+
